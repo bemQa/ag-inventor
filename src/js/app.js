c32e9485e206e833ab4888e45285ded35ea0b677
Sergey Kiselev <s.kiselev@dataloft.ru>
2020-02-22 01:43:07 +0300

    Main page adaptive and mobile; Start work on popups

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,64 @@ import "../css/entry.scss"
 import $ from 'jquery'
 import {initDevState} from "./dev";
 import '../img/common/mondelez.png';
+import '../img/main/mobile/ag-logo.png';
 
 $(document).ready(_ => {
+
   initDevState();
+
+  setPageSizeTypes();
+  stickFooter();
+
+  bindListeners();
 });
 
-$(window).load($('body').css({
-  opacity: 1
-}));
+function bindListeners() {
 
+  $(window).on('resize', _ => {
+    setPageSizeTypes();
+    stickFooter();
+  });
+
+  $('[data-popup]').on('click', e => {
+    showPopup($(e.target).attr('data-popup'));
+  });
+
+  $('.popup, .popup-close').on('click', e => {
+    $('.popup').removeClass('active');
+    $('body').removeClass('no-scroll');
+  });
+
+  $('.popup-content').on('click', e => {
+    e.stopPropagation();
+  });
+
+}
+
+function setPageSizeTypes() {
+  window.isMobile = window.innerWidth < 780;
+  window.isAdaptive = window.innerWidth < 1200;
+}
+
+function stickFooter() {
+
+  let windowBodyHeightsDifference = window.innerHeight - $('body').innerHeight(),
+      footer = $('footer');
+
+  if (window.isAdaptive) {
+
+    if (windowBodyHeightsDifference > 0) {
+      footer.css('top', `${windowBodyHeightsDifference - 12}px`);
+    } else {
+      footer.css('top', 0);
+    }
+  } else {
+    footer.css('top', 0);
+  }
+}
+
+function showPopup(type) {
+  $('body').addClass('no-scroll');
+  $('.popup-content.active').removeClass('active');
+  $(`.popup, .popup-content--${type}`).addClass('active');
+}

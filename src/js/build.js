fdc346172bf14e11e5cfc15c6cd25c6ea243a02a
Sergey Kiselev <s.kiselev@dataloft.ru>
2020-02-25 16:56:56 +0300

    Constructor process

--- src/js/build.js
+++ src/js/build.js
@@ -19,6 +19,12 @@ export function BlockbusterBuilder() {
     this.step = null;
     this.addingIsLocked = false;
     this.setIngredients();
+
+    this.firstStepState = {
+      currentCategory: null,
+      currentProduct: null
+    };
+
     this.isDemo =
         ~location.href.indexOf('localhost') ||
         ~location.href.indexOf('.world');
@@ -27,6 +33,7 @@ export function BlockbusterBuilder() {
   this.bindPageElements = function () {
     this.wrap = $('.build-content');
     this.page = {
+      container: this.f('.container').eq(0),
       steps: {
         wrap: this.f('.build--slides-numbers-wrap'),
         circles: {
@@ -151,6 +158,11 @@ export function BlockbusterBuilder() {
     this.page.submitButton.on('click', _ => {
       this.submitBuild();
     });
+
+    this.page.selectColorSlide.showMoreButton.on('click', _ => {
+      this.page.selectColorSlide.showMoreButton.hide();
+      this.page.selectColorSlide.elements.show();
+    });
   };
 
   this.load = function () {
@@ -174,15 +186,6 @@ export function BlockbusterBuilder() {
 
     }
 
-    // Show elements, for any step
-    {
-      this.page.steps.circles[`number_${this.step}`].addClass('circle-filled');
-      this.page.steps.text[`number_${this.step}`].addClass('step-selected');
-      this.page.steps.wrap.show();
-
-      this.page.floatUi.wrap.show();
-    }
-
     stickFooter();
   };
 
@@ -192,13 +195,15 @@ export function BlockbusterBuilder() {
       case 1:
         this.page.cupBg.show();
 
-        if (this.isDemo) {
-          this.page.sliderBody.main.title.html('Клубника');
-          this.page.sliderBody.main.content
-              .directionButtonsWrap.left.title.html('Апельсин');
-          this.page.sliderBody.main.content
-              .directionButtonsWrap.right.title.html('Малина');
-        }
+        this.fillProductsSlider();
+
+        // if (this.isDemo) {
+        //   this.page.sliderBody.main.title.html('Клубника');
+        //   this.page.sliderBody.main.content
+        //       .directionButtonsWrap.left.title.html('Апельсин');
+        //   this.page.sliderBody.main.content
+        //       .directionButtonsWrap.right.title.html('Малина');
+        // }
 
         this.page.sliderBody.categories.wrap.show();
         this.page.sliderBody.main.wrap.show();
@@ -217,6 +222,14 @@ export function BlockbusterBuilder() {
       case 2:
         this.page.cupBg.show();
 
+        this.page.container.removeClass('steps-3-4');
+
+        this.page.selectColorSlide.wrap.hide();
+        this.page.floatExampleResult.wrap.hide();
+        this.page.sliderBody.categories.wrap.hide();
+
+        this.fillChocolateSlider();
+
         if (this.isDemo) {
           this.page.sliderBody.main.title.html('Молочный шоколад');
           this.page.sliderBody.main.content
@@ -225,6 +238,7 @@ export function BlockbusterBuilder() {
               .directionButtonsWrap.right.title.html('Белый <br> шоколад');
         }
 
+        this.page.sliderBody.wrap.removeClass('steps-3-4');
         this.page.sliderBody.main.wrap.show();
 
         this.page.floatUi.buttons.left.show();
@@ -245,15 +259,29 @@ export function BlockbusterBuilder() {
       case 3:
         this.page.cupBg.hide();
 
+        this.page.container.addClass('steps-3-4');
+
         this.page.selectColorSlide.wrap.show();
         this.page.floatExampleResult.wrap.show();
+        this.page.finalSlide.wrap.hide();
 
-        this.page.sliderBody.categories.wrap.hide();
         this.page.sliderBody.main.wrap.hide();
-        this.page.sliderBody.wrap.show();
+
+        this.page.sliderBody.wrap
+            .addClass('steps-3-4')
+            .removeClass('step-4')
+            .show();
 
         this.page.floatUi.buttons.left.show();
         this.page.floatUi.buttons.right.show();
+
+        this.page.floatUi.variants.all
+            .removeClass('filled')
+            .attr('style', '');
+
+        this.page.floatUi.variants.variant_1.show();
+        this.page.floatUi.variants.variant_2.hide();
+        this.page.floatUi.variants.variant_3.hide();
 
         this.page.submitButton.text('Сохранить прогресс');
         this.page.floatUi.variants.wrap.show();
@@ -262,12 +290,42 @@ export function BlockbusterBuilder() {
       case 4:
         this.page.cupBg.hide();
 
+        this.page.container.addClass('steps-3-4');
+
+        this.page.selectColorSlide.wrap.hide();
+        this.page.finalSlide.wrap.show();
+        this.page.floatExampleResult.wrap.show();
+
         this.page.floatUi.buttons.left.show();
         this.page.floatUi.buttons.right.hide();
+
+        this.page.floatUi.variants.all
+            .removeClass('filled')
+            .attr('style', '');
+
+        this.page.sliderBody.wrap
+            .addClass('steps-3-4')
+            .addClass('step-4')
+            .show();
+
+        this.page.floatUi.variants.variant_1.hide();
+        this.page.floatUi.variants.variant_2.hide();
+        this.page.floatUi.variants.variant_3.hide();
 
         this.page.submitButton.text('Сохранить и создать свой блокбастер вкуса');
         this.page.floatUi.variants.wrap.hide();
     }
+
+    this.page.steps.circles.all.removeClass('circle-filled');
+    this.page.steps.text.all.removeClass('step-selected');
+
+    this.page.steps.circles[`number_${this.step}`].addClass('circle-filled');
+    this.page.steps.text[`number_${this.step}`].addClass('step-selected');
+    this.page.steps.wrap.show();
+
+    this.page.floatUi.wrap.show();
+
+    stickFooter();
   };
 
   this.addCurrentProductToBuild = function () {
@@ -275,7 +333,7 @@ export function BlockbusterBuilder() {
     let ingredientsWithThisProduct = this.page.floatUi.variants.all
         .filter((index, variant) => {
           return $(variant).attr('style') ===
-              `background-image: url('${this.page.sliderBody.main.content.scene.element.attr('data-small-img')}')`
+              `background-image: url('${this.firstStepState.currentProduct.img.inCart}')`
         });
 
     if (ingredientsWithThisProduct.length ||
@@ -292,8 +350,8 @@ export function BlockbusterBuilder() {
     )
         .addClass('filled')
         .attr(
-        'style',
-        `background-image: url('${this.page.sliderBody.main.content.scene.element.attr('data-small-img')}')`);
+            'style',
+            `background-image: url('${this.firstStepState.currentProduct.img.inCart}')`);
 
     setTimeout(_ => {
       this.addingIsLocked = false;
@@ -317,6 +375,62 @@ export function BlockbusterBuilder() {
     }
     this.step++;
     this.loadByStep();
+  };
+
+  this.fillProductsSlider = function () {
+
+    if (this.firstStepState.currentCategory === null) {
+      this.firstStepState.currentCategory = 'Фрукты';
+    }
+
+    let currentCategoryParts = this.parts.filter(part => {
+      return part.category === this.firstStepState.currentCategory;
+    });
+
+    if (currentCategoryParts.length === 0) {
+      throw Error('Категория ингредиентов не может быть пустой');
+    }
+
+    if (currentCategoryParts.length === 1) {
+
+      this.firstStepState.currentProduct = currentCategoryParts[0];
+
+      this.page.sliderBody.main.content.directionButtonsWrap.left.wrap.hide();
+      this.page.sliderBody.main.content.directionButtonsWrap.right.wrap.hide();
+
+      this.page.sliderBody.main.content.scene.element.attr('style',
+          `background-image: url('${this.firstStepState.currentProduct.img.full}')`);
+
+    } else if (currentCategoryParts.length >= 2) {
+
+      this.firstStepState.currentProduct = currentCategoryParts[1];
+
+      this.page.sliderBody.main.content.scene.element.attr('style',
+          `background-image: url('${this.firstStepState.currentProduct.img.full}')`);
+
+      this.page.sliderBody.main.content
+          .directionButtonsWrap.left.title.html(currentCategoryParts[0].title);
+      this.page.sliderBody.main.content
+          .directionButtonsWrap.left.image.attr('style',
+          `background-image: url('${currentCategoryParts[0].img.arrow}')`);
+      this.page.sliderBody.main.content.directionButtonsWrap.left.wrap.show();
+
+      if (currentCategoryParts.length > 2) {
+
+        this.page.sliderBody.main.content
+            .directionButtonsWrap.right.title.html(currentCategoryParts[2].title);
+        this.page.sliderBody.main.content
+            .directionButtonsWrap.right.image.attr('style',
+            `background-image: url('${currentCategoryParts[2].img.arrow}')`);
+        this.page.sliderBody.main.content.directionButtonsWrap.right.wrap.show();
+      } else {
+        this.page.sliderBody.main.content.directionButtonsWrap.right.wrap.hide();
+      }
+    }
+  };
+
+  this.fillChocolateSlider = function () {
+
   };
 
   this.submitBuild = function () {
@@ -356,8 +470,34 @@ export function BlockbusterBuilder() {
   };
 
   this.setIngredients = function () {
-    this.categories = productsJSON.categories;
-    this.parts = productsJSON.ingredients;
+
+    const productsBlock = $('.product-info');
+
+    let categories = [];
+    let products = [];
+
+    productsBlock.each((index, element) => {
+      element = $(element);
+
+      if (!~categories.indexOf(element.data('productCategory'))) {
+        categories.push(element.data('productCategory'));
+      }
+
+      products.push({
+        code: element.data('productCode'),
+        title: element.data('productTitle'),
+        category: element.data('productCategory'),
+        img: {
+          full: element.data('productImgFull'),
+          forAddNewAnimation: element.data('productImgForAddAnimation'),
+          arrow: element.data('productImgArrow'),
+          inCart: element.data('productImgInCart'),
+        }
+      });
+    });
+
+    this.categories = categories;
+    this.parts = products;
   };
 }
 
--- src/js/images_import.js
+++ src/js/images_import.js
@@ -6,10 +6,14 @@ import '../img/examples/fruit-02-lemon.png';
 import '../img/examples/fruit-03-strawberry.png';
 
 import '../img/examples/construct-product-icon-01-strawberry.png';
-import '../img/examples/construct-product-icon-03-orange.png';
 import '../img/examples/construct-product-icon-02-raspberry.png';
+import '../img/examples/construct-product-icon-03-orange.png';
 
 import '../img/examples/construct-slide-product-01.png';
+
+import '../img/examples/result-1-raspberry.png';
+import '../img/examples/result-2-strawberry.png';
+import '../img/examples/result-3-lemon.png';
 
 import '../img/common/errors-404.png';
 import '../img/common/errors-500.png';
